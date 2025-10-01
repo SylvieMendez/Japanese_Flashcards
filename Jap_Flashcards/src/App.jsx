1f7fed868632a6f4416ad175fe92fa91a7439b71
sylvie <sylviemendez21@gmail.com>
2025-09-30 22:36:16 -0400

    stored cards data in a array

--- Jap_Flashcards/src/App.jsx
+++ Jap_Flashcards/src/App.jsx
@@ -6,56 +6,33 @@ const App = () => {
   const [click, whenClicked] = useState(0);
   const updateClick = () => whenClicked(click);
 
+  const cardData = [
+    {id: 1, question: 'Hello', answer: 'Konnichiwa'},
+    {id: 2, question: 'Goodbye', answer: 'Sayonara'},
+    {id: 3, question: 'Thank you', answer: 'Arigatou'},
+    {id: 4, question: 'Yes', answer: 'Hai'},
+    {id: 5, question: 'No', answer: 'Iie'},
+    {id: 6, question: 'Please', answer: 'Kudasai'},
+    {id: 7, question: 'Excuse me / I\'m sorry', answer: 'Sumimasen'},
+    {id: 8, question: 'Good morning', answer: 'Ohayou gozaimasu'},
+    {id: 9, question: 'Good night', answer: 'Oyasuminasai'},
+    {id: 10, question: 'How are you?', answer: 'Ogenki desu ka?'}
+  ];
+
   return (
     <div className="App">
       <div className="header">
         <h1>Japanese Flashcards</h1>
         <h2>Want to know how to speak simple Japanese? Lets learn!</h2>
-        <p>Number of cards: 10</p>
+        <p>Number of cards: {cardData.length}</p>
       </div>
       <div className="cards" onClick={updateClick}>
         <Cards 
-          question="Hello"
-          answer="Konnichiwa"
-        />
-        <Cards 
-          question="Goodbye"
-          answer="Sayonara"
-        />
-        <Cards 
-          question="Thank you"
-          answer="Arigatou"
-        />
-        <Cards 
-          question="Yes"
-          answer="Hai"
-        />
-        <Cards 
-          question="No"
-          answer="Iie"
-        />
-        <Cards 
-          question="Please"
-          answer="Onegaishimasu"
-        />
-        <Cards 
-          question="Excuse me / I'm sorry"
-          answer="Sumimasen"
-        />
-        <Cards 
-          question="Good morning"
-          answer="Ohayou gozaimasu"
-        />
-        <Cards 
-          question="Good night"
-          answer="Oyasuminasai"
-        />
-        <Cards 
-          question="Good afternoon"
-          answer="Kunbanwa"
+          key={cardData.id}
+          question={cardData.question}
+          answer={cardData.answer}
         />
       </div>
-
     </div>
   )
 }
